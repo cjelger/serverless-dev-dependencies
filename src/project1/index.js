11b25075b5e973a410a1e0e9257304ed5dbeefaa
Christophe Jelger <jelger@adobe.com>
2017-06-23 14:01:01 +0200

    Show issue with serverless dev dependencies

---
+++ src/project1/index.js
@@ -0,0 +1,6 @@
+
+function project1() {
+  console.log('project1');
+}
+
+module.export.main = project1;
---
+++ src/project2/index.js
@@ -0,0 +1,6 @@
+
+function project2() {
+  console.log('project2');
+}
+
+module.export.main = project2;
